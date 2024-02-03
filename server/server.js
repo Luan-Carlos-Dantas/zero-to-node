adf992b12c7d021507d97a5299fc3b76a60d5561
Luan Carlos Dantas dos Santos <luancarlos492@gmail.com>
2024-02-03 20:49:47 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,73 @@
+import fastify from "fastify";
+// import { DatabaseMemory } from "../db/databaese-memory.js";
+import { DatabasePostgres } from "../db/database-postgres.js";
+
+const server = fastify()
+
+// const db = new DatabaseMemory()
+const db = new DatabasePostgres
+
+// GET / POST / PUT / DELETE / PATCH
+
+server.get('/videos', async (req) =>{
+  const search = req.query.search
+
+  const videos = await db.list(search)
+
+  return videos
+})
+
+// Request Body
+
+server.post('/videos', async (req, rep)=>{
+  const {title, description, duration} = req.body
+
+
+  await db.create({
+    title: title,
+    description: description,
+    duration: duration
+  })
+
+  console.log(db.list())
+  return rep.status(201).send()
+})
+
+// Route Parameter
+
+server.put('/videos/:id', async (req, rep)=>{
+  const videoID = req.params.id
+  const {title, description, duration} = req.body
+  await db.update(videoID,{
+    title: title,
+    description: description,
+    duration: duration
+  })
+
+  return rep.status(204).send()
+})
+
+server.delete('/videos/:id', async (req, rep)=>{
+  const videoID = req.params.id
+
+  await db.delete(videoID)
+
+  return rep.status(204).send()
+})
+
+server.listen({
+  port:process.env.PORT ?? 8000
+})
+
+
+
+
+// import {createServer} from 'node:http'
+
+// const server = createServer((req,res)=>{
+//   res.write('salve')
+
+//   return res.end()
+// })
+
+// server.listen(8000)
